700e640c89c38d35bf8ae93d50157630aee2b4cc
hdktvd <hardiktrivedi974@gmail.com>
2024-05-27 11:07:12 +0530

    send funds from wallet via transaction

--- src/main/java/com/hdktvd/blockchain/wallet/Wallet.java
+++ src/main/java/com/hdktvd/blockchain/wallet/Wallet.java
@@ -2,13 +2,20 @@ package com.hdktvd.blockchain.wallet;
 
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
+import com.hdktvd.blockchain.DumbCoin;
+import com.hdktvd.blockchain.transaction.Transaction;
+import com.hdktvd.blockchain.transaction.TransactionInput;
 import com.hdktvd.blockchain.transaction.TransactionOutput;
 
 public class Wallet {
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
+	
+	public HashMap<String, TransactionOutput> UTXOs = new HashMap<String, TransactionOutput>();
 		
 	public Wallet() {
 		generateKey();
@@ -30,5 +37,46 @@ public class Wallet {
 		}
 	}
 	
+	public float getBalance() {
+		float total = 0;
+//		populate the wallet with global UTXO map by matching publickey
+		for (Map.Entry<String, TransactionOutput> item : DumbCoin.UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			if (UTXO.isMine(publicKey)) {
+				UTXOs.put(UTXO.id, UTXO);
+				total += UTXO.value;
+			}
+		}
+		return total;
+	}
 	
+	public Transaction sendFunds(PublicKey receiver, float value) {
+		if (getBalance() < value) {
+			System.out.println("Unsufficient balance. Transaction discarded.");
+			return null;
+		}
+		
+//		generate all the input txns from unspent output txns until the required amount is reached.
+		float total = 0;
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		for (Map.Entry<String, TransactionOutput> item : UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			inputs.add(new TransactionInput(UTXO.id));
+			total += UTXO.value;
+			if (total >= value) {
+				break;
+			}
+		}
+		
+		Transaction transaction = new Transaction(publicKey, receiver, value, inputs);
+//		sign the transaction with sender's private key such that it can be validated on receive
+		transaction.createSignature(privateKey);
+		
+//		remove inputs txns from UTXO list as those will be spent later
+		for ( TransactionInput ti : inputs ) {
+			DumbCoin.UTXOs.remove(ti.transactionOutputId);
+		}
+		
+		return transaction;
+	}
 }
